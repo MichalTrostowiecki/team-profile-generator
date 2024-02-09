9791a47412d4d35a4758b9ddf0013778638880d5
Michal_T <michal.trostowiecki@gmail.com>
2024-02-09 14:19:06 +0000

    Add prompts for user to build team members and to store them

--- index.js
+++ index.js
@@ -12,4 +12,143 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const managerPrompt = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter Manager's name"
+    },
+    {
+        type: "input",
+        name: "employeeId",
+        message: "Enter Employee ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter Manager's email adress"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter office number"
+    }
 
+]
+
+const userChoicePrompt = [
+    {
+        type: "list",
+        name: "userChoice",
+        message: "What do you want to do now?",
+        choices: ["Add an engineer", "Add an intern", "Finish building Team"]
+    }
+]
+
+const engineerPrompt = [  
+    {
+        type: "input",
+        name: "name",
+        message: "Enter engineer's name"
+    },
+    {
+        type: "input",
+        name: "employeeId",
+        message: "Enter Engineer's ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter Engineer's email adress"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter Github username"
+    }
+]
+
+const internPrompt = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter Intern's name"
+    },
+    {
+        type: "input",
+        name: "employeeId",
+        message: "Enter Intern's ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter Intern's email adress"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Enter school"
+    }
+]
+
+
+
+
+async function init() {
+    const team = []
+    let assigningTeam = true;
+    
+    const managerDetails = await inquirer.prompt(managerPrompt);
+
+    const manager = new Manager(
+        managerDetails.name,
+        managerDetails.employeeId,
+        managerDetails.email,
+        managerDetails.officeNumber
+    );
+    
+    team.push(manager);
+
+    while (assigningTeam) {
+        const userSelection = await inquirer.prompt(userChoicePrompt);
+
+        switch (userSelection.userChoice) {
+
+            case "Add an engineer":
+                const engineerAnswers = await inquirer.prompt(engineerPrompt);
+                const engineer = new Engineer(
+                    engineerAnswers.name,
+                    engineerAnswers.employeeId,
+                    engineerAnswers.email,
+                    engineerAnswers.github
+                )
+                team.push(engineer);
+                break;
+            
+            case "Add an intern":
+                const internAnswers = await inquirer.prompt(internPrompt);
+                const intern = new Intern(
+                    internAnswers.name,
+                    internAnswers.employeeId,
+                    internAnswers.email,
+                    internAnswers.school
+                )
+                team.push(intern);
+                break;
+
+            case "Finish building Team":
+                assigningTeam = false;
+                break;
+       }
+    }
+
+    console.log(team)
+    
+
+
+}
+
+init();
+
+
+//Add an engineer", "Add an intern", "Finish building Team
