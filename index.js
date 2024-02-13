b7c6172a2a053e8d5542e3d59669f654d6ac2b08
Michal_T <michal.trostowiecki@gmail.com>
2024-02-13 15:47:49 +0000

    Add descriptive comments to index.js

--- index.js
+++ index.js
@@ -24,13 +24,18 @@ const internPrompt = require("./src/questions/internPrompt");
 
 
 
-
+// Main function to run the application
 async function init() {
+
+    // Empty array to hold team-members
     const team = []
+    // This is used to control while loop for adding team members
     let assigningTeam = true;
     
+    // Prompt to ask for manager's details
     const managerDetails = await inquirer.prompt(managerPrompt);
 
+    // Create a new Manager object with details provided by the user
     const manager = new Manager(
         managerDetails.name,
         managerDetails.employeeId,
@@ -38,11 +43,14 @@ async function init() {
         managerDetails.officeNumber
     );
     
+    // Add manager to team array
     team.push(manager);
 
+    // Keep prompting user to add to team until they choose to finish build their team
     while (assigningTeam) {
         const userSelection = await inquirer.prompt(userChoicePrompt);
 
+        // Based on what user choose, continue with appropriate action
         switch (userSelection.userChoice) {
 
             case "Add an engineer":
@@ -73,12 +81,14 @@ async function init() {
        }
     }
 
+    // Generate HTML with team members added by the user
     const htmlTemplate = render(team);
 
+    // Write HTML to file and place in specified folder
     fs.writeFile(outputPath, htmlTemplate, (err) => {
         err ? console.error(err) : console.log("Sucess");
     })
 }
 
-
+// Run the application
 init();
